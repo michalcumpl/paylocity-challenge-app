eeda420a8f86f3c770e84e8742445017b59598e8
Michal Cumpl <oswmcumpl@oriflame.com>
2025-10-30 15:18:55 +0100

    Add mock

---
+++ src/mocks/employees.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import type { Employee, Dependent } from '../types';
+
+function makeDependent(): Dependent {
+  return {
+    id: faker.string.uuid(),
+    name: faker.person.firstName() + ' ' + faker.person.lastName(),
+  };
+}
+
+function makeEmployee(): Employee {
+  const depCount = faker.number.int({ min: 0, max: 3 });
+  const dependents: Dependent[] = Array.from({ length: depCount }, () => makeDependent());
+  return {
+    id: faker.string.uuid(),
+    name: faker.person.firstName() + ' ' + faker.person.lastName(),
+    dependents,
+  };
+}
+
+export const mockEmployees: Employee[] = Array.from({ length: 100 }, () => makeEmployee());
+
